98894da82eb7a7b91dad3ae6e12a71e9809aef11
Oleg991 <o.n.eremenko@gmail.com>
2024-03-25 19:48:35 +0300

    Добавил кнопки на экран "Ещё"

--- SwiftUI-Days/Screens/MoreScreen.swift
+++ SwiftUI-Days/Screens/MoreScreen.swift
@@ -6,23 +6,60 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MoreScreen: View {
+    private let easyDevLink = URL(string: "https://t.me/easy_dev991")
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+    @State private var showDeleteDataConfirmation = false
+    
     var body: some View {
-        VStack(spacing: 12) {
+        VStack(spacing: 20) {
             feedbackButton
+            developerProfileButton
+            if !items.isEmpty {
+                removeAllDataButton
+            }
         }
+        .buttonStyle(.borderedProminent)
+        .foregroundStyle(.buttonTint)
+        .animation(.default, value: items.isEmpty)
     }
     
     private var feedbackButton: some View {
         Button("Send feedback", action: FeedbackSender.sendFeedback)
-            .buttonStyle(.borderedProminent)
-            .foregroundStyle(.buttonTint)
+    }
+    
+    @ViewBuilder
+    private var developerProfileButton: some View {
+        if let easyDevLink {
+            Link(destination: easyDevLink) {
+                Text("App Developer")
+            }
+        }
+    }
+    
+    private var removeAllDataButton: some View {
+        Button("Delete all data") {
+            showDeleteDataConfirmation.toggle()
+        }
+        .transition(.slide.combined(with: .scale))
+        .confirmationDialog(
+            "Do you want to delete all data permanently?",
+            isPresented: $showDeleteDataConfirmation,
+            titleVisibility: .visible
+        ) {
+            Button("Delete", role: .destructive) {
+                try? modelContext.delete(model: Item.self)
+            }
+        }
     }
 }
 
 #if DEBUG
 #Preview {
     MoreScreen()
+        .modelContainer(PreviewModelContainer.make(with: Item.makeList()))
 }
 #endif
