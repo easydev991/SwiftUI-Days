369418a972cc60fb58deca0960c75221a21d812a
Oleg991 <o.n.eremenko@gmail.com>
2024-11-10 12:53:23 +0300

    Добавил unit-тесты

---
+++ SwiftUI-DaysTests/SwiftUI_DaysTests.swift
@@ -0,0 +1,48 @@
+//
+//  SwiftUI_DaysTests.swift
+//  SwiftUI-DaysTests
+//
+//  Created by Oleg991 on 10.11.2024.
+//
+
+import Testing
+import Foundation
+@testable import SwiftUI_Days
+
+@Suite("Item tests")
+struct SwiftUI_DaysTests {
+    /// Тестируем инициализацию объекта Item
+    @Test func itemInitialization() {
+        let title = "Test Title"
+        let details = "Test Details"
+        let oneDayAgo = Date(timeIntervalSinceNow: -86400)
+        let item = Item(title: title, details: details, timestamp: oneDayAgo)
+        #expect(item.title == title)
+        #expect(item.details == details)
+        #expect(item.timestamp == oneDayAgo)
+    }
+
+    /// Тестируем `daysCount`, когда событие произошло только что
+    @Test func daysCountWithNoDaysPassed() {
+        let item = Item(title: "Recent Event", timestamp: .now)
+        #expect(item.daysCount == 0)
+    }
+
+    /// Тестируем `daysCount` для события, произошедшего 1 день назад
+    @Test func daysCountWithOneDayPassed() {
+        let oneDayOldItem = Item(
+            title: "One Day Ago",
+            timestamp: Date(timeIntervalSinceNow: -86400)
+        )
+        #expect(oneDayOldItem.daysCount == 1)
+    }
+
+    /// Тестируем `daysCount` для события, произошедшего 5 дней назад
+    @Test func daysCountWithMultipleDaysPassed() {
+        let fiveDaysOldItem = Item(
+            title: "Five Days Ago",
+            timestamp: Date(timeIntervalSinceNow: -432000)
+        )
+        #expect(fiveDaysOldItem.daysCount == 5)
+    }
+}
