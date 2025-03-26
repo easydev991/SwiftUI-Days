6902e1ebac44b404bad54a04cdd9c5001faabc8c
Oleg991 <51740599+easydev991@users.noreply.github.com>
2025-03-26 09:39:19 +0300

    Убрал лишнее из UI-тестов

--- SwiftUI-DaysUITests/SwiftUI_DaysUITests.swift
+++ SwiftUI-DaysUITests/SwiftUI_DaysUITests.swift
@@ -7,11 +7,11 @@
 
 import XCTest
 
+@MainActor
 final class SwiftUI_DaysUITests: XCTestCase {
     private let springBoard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
     private var app: XCUIApplication!
 
-    @MainActor
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
@@ -20,7 +20,6 @@ final class SwiftUI_DaysUITests: XCTestCase {
         app.launch()
     }
 
-    @MainActor
     override func tearDown() {
         super.tearDown()
         app.launchArguments.removeAll()
@@ -28,23 +27,15 @@ final class SwiftUI_DaysUITests: XCTestCase {
     }
     
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 }
